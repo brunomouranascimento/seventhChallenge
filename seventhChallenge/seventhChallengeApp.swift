fe1a2cb9c020aba59b2f402677b2a14852c54c58
Bruno Nascimento <bruno.bmn@gmail.com>
2022-05-06 14:18:29 -0300

    Initial Commit

---
+++ seventhChallenge/seventhChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  seventhChallengeApp.swift
+//  seventhChallenge
+//
+//  Created by Bruno Nascimento on 06/05/22.
+//
+
+import SwiftUI
+
+@main
+struct seventhChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
